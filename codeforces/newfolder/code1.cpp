b0abf4ad28774c622b21976e5d0a55e149d8dc7e
Aditya Prem <aditya.prem2003@gmail.com>
2025-03-25 23:59:34 +0530

    aditya

---
+++ codeforces/newfolder/code1.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f(i, a, b) for(int i=a;i<b;i++)
+#define all(x) x.begin(),x.end()
+#define vprint(v) for (auto& elem : v) cout << elem << " "; cout << endl;
+#define vmatprint(vv) for (auto& row : vv) { for (auto& elem : row) cout << elem << " "; cout << endl; } cout << endl;
+#define mprint(m) for (auto it : m) cout << it.first << " : " << it.second << endl; cout << endl;
+#define vint vector<int>
+#define vstring vector<string>
+#define vmat vector<vector<int>>
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mii map<int,int>
+#define mll map<ll,ll>
+
+const int INF = 1e9;
+const ll MOD = 1e9+7;
+
+int main() {
+    FAST_IO;
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vint nums;
+        while(n--){
+            int x;
+            cin>>x;
+            nums.push_back(x);
+        }
+
+        mii mp;
+        int cnt=0;
+        bool flag=false;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+            cnt++;
+            if(mp[0]>=3&&mp[1]>=1&&mp[2]>=2&&mp[3]>=1&&mp[5]>=1){
+                flag=true;
+                break;
+            }
+        }
+        if(flag==true){
+            cout<<cnt<<endl;
+        }
+        else{
+            cout<<0<<endl;
+        }
+    }
+    return 0;
+}
+//by ad73prem
---
+++ codeforces/newfolder/code2.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f(i, a, b) for(int i=a;i<b;i++)
+#define all(x) x.begin(),x.end()
+#define vprint(v) for (auto& elem : v) cout << elem << " "; cout << endl;
+#define vmatprint(vv) for (auto& row : vv) { for (auto& elem : row) cout << elem << " "; cout << endl; } cout << endl;
+#define mprint(m) for (auto it : m) cout << it.first << " : " << it.second << endl; cout << endl;
+#define vint vector<int>
+#define vstring vector<string>
+#define vmat vector<vector<int>>
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mii map<int,int>
+#define mll map<ll,ll>
+
+const int INF = 1e9;
+const ll MOD = 1e9+7;
+
+int main() {
+    FAST_IO;
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        vint nums;
+        int mx=INT_MIN;
+        int mn=INT_MAX;
+        for(int i=0;i<n;i++){
+            int y;
+            cin>>y;
+            mx=max(mx,y);
+            mn=min(mn,y);
+            nums.push_back(y);
+        }
+
+        sort(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.end());
+        // vprint(nums);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(nums[0]>=x){
+                cnt++;
+                nums.erase(nums.begin());
+                // vprint(nums);
+            }
+
+            // cout<<"i : "<<i<<" nums : ";
+            // vprint(nums);
+            
+        }
+        // cout<<"coutnt : "<<cnt<<endl;
+        // vprint(nums);
+
+        int cnt2=0;
+        int mn2=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            cnt2++;
+            mn2=min(mn2,nums[i]);
+            if(cnt2*mn2>=x){
+                cnt++;
+                cnt2=0;
+                mn2=INT_MAX;
+                
+                
+            }
+            
+        }
+        cout<<cnt<<endl;
+
+
+    }
+    return 0;
+}
+//by ad73prem

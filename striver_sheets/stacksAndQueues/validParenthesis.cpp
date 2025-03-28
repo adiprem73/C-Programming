7e142ee65eabcb96c775b70769ca0e5036b3b6c2
Aditya Prem <aditya.prem2003@gmail.com>
2025-03-28 12:36:05 +0530

    afternoon commit

---
+++ striver_sheets/stacksAndQueues/validParenthesis.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f(i, a, b) for(int i=a;i<b;i++)
+#define all(x) x.begin(),x.end()
+#define vprint(v) for (auto& elem : v) cout << elem << " "; cout << endl;
+#define vmatprint(vv) for (auto& row : vv) { for (auto& elem : row) cout << elem << " "; cout << endl; } cout << endl;
+#define mprint(m) for (auto it : m) cout << it.first << " : " << it.second << endl; cout << endl;
+#define vint vector<int>
+#define vstring vector<string>
+#define vmat vector<vector<int>>
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mii map<int,int>
+#define mll map<ll,ll>
+
+const int INF = 1e9;
+const ll MOD = 1e9+7;
+
+class MinStack{
+    private:
+        vector<pair<int,int>> s;
+    
+    public:
+    MinStack() {}  //constructor doesnt need ot doanythig
+    
+    void push(int val) {
+        if(s.empty()){
+            s.emplace_back(val,val);
+        }
+        else{
+            int currentMin=min(s.back().second,val);
+            s.emplace_back(val,currentMin);
+        }
+    }
+    
+    void pop() {
+        s.pop_back();
+    }
+    
+    int top() {
+        return s.back().first;
+    }
+    
+    int getMin() {
+        return s.back().second;
+    }
+};
+
+bool isValid(string s) {
+        
+}
+
+int main() {
+    FAST_IO;
+    
+    return 0;
+}
+//by ad73prem

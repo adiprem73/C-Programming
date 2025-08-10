360f70f9d57c489ed62091025ae8e2084bd73a2a
Aditya Prem <aditya.prem2003@gmail.com>
2025-08-10 22:54:31 +0530

    ello

---
+++ striver_sheets/graphs/graph.cpp
@@ -0,0 +1,38 @@
+//n: no. o fnodes
+//m: no. o fedges
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define f(i, a, b) for(int i=a;i<b;i++)
+#define all(x) x.begin(),x.end()
+#define vprint(v) for (auto& elem : v) cout << elem << " "; cout << endl;
+#define mprint(m) for (auto it:m) cout<<it.first<<" : "<<it.second<<endl; cout<<endl;
+#define vint vector<int>
+#define vstring vector<string>
+#define vmat vector<vector<int>>
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mii map<int,int>
+#define mll map<ll,ll>
+
+const int INF = 1e9;
+const ll MOD = 1e9+7;
+
+int main() {
+    FAST_IO;
+    int n,m;//n: no. o fnodes
+    cin>>n>>m;//m: no. o fedges
+    int adj[n+1][m+1];
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u][v]=1;
+        adj[v][u]=1;
+    }
+    //entire graph has been store from the user
+    return 0;
+}
+//by ad73prem

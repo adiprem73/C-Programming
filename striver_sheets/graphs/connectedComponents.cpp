9c7951a7c924170dda85cf42cc4c8ef7d3a9fd18
Aditya Prem <aditya.prem2003@gmail.com>
2025-08-12 10:54:40 +0530

    hello

--- striver_sheets/graphs/connectedComponents.cpp
+++ striver_sheets/graphs/connectedComponents.cpp
@@ -18,11 +18,38 @@ using namespace std;
 const int INF = 1e9;
 const ll MOD = 1e9+7;
 
+void dfs(int node, vector<vector<int>>& adjls, vector<int> & vis){
+    vis[node]=1;
+    for(auto it: adjls[node]){
+        if(!vis[it]){
+            dfs(it, adjls, vis);
+        }
+    }
+}
+
+int findNumberOfComponent(int V, vector<vector<int>> &edges) {
+    vector<vector<int>> adjls(V);
+    for(int i=0;i<edges.size();i++){
+        adjls[edges[i][0]].push_back(edges[i][1]);
+        adjls[edges[i][1]].push_back(edges[i][0]);
+    }
+    vector<int> vis(V,0);
+    int cnt=0;
+    for(int i=0;i<V;i++){
+        if(!vis[i]){
+            cnt++;
+            dfs(i, adjls, vis);
+        }
+    }
+    return cnt;
+}
+
 int main() {
     FAST_IO;
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    vmat mt={{0,1},{1,2}};
+    cout<<findNumberOfComponent(4, mt);
     return 0;
 }
 //by ad73prem

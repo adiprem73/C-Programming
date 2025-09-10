68901ab664578f96defdfcf2b19d15b003e363bf
Aditya Prem <aditya.prem2003@gmail.com>
2025-09-11 04:17:37 +0530

    night commit

--- striver_sheets/LinkedList/sortLL.cpp
+++ striver_sheets/LinkedList/sortLL.cpp
@@ -26,6 +26,52 @@ struct ListNode
 };
 
 
+ListNode* findMiddle(ListNode* head){
+    ListNode* slow=head;
+    ListNode* fast=head->next;
+
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
+ListNode* merge(ListNode* node1, ListNode* node2){
+    //this is the part where we would merge two linked lists in merge sort fashio
+    ListNode* dummy= new ListNode(0); //we often take a dummy list starting from 0 to properly store the answer. we will return dummy->next at the end of the program
+
+    ListNode* temp=dummy; //this will basiclaly be used as a ptr to traverse on dummy
+    while(node1 && node2){
+        if(node1->val<node2->val){
+            temp->next=node1;
+            node1=node1->next;
+        }else{
+            temp->next=node2;
+            node2=node2->next;
+        }
+        temp=temp->next; //after every assignment we move the temp forward
+    }
+    temp->next = (node1) ? node1 : node2;// this will append any remaining node. there will be atmoset 1 remaining node
+    return dummy->next;
+}
+
+
+ListNode* sortlist(ListNode *head){
+
+    if(!head || !head->next)return head; //if the ll is empty or has only one node then return that only
+
+    ListNode* mid= findMiddle(head);
+    // cout<<mid->val<<endl;
+    ListNode* righthalf= mid->next;
+    mid->next= nullptr; //to split them into two halves
+
+    ListNode* leftSorted= sortlist(head);
+    ListNode* rightSorted= sortlist(righthalf);
+
+    return merge(leftSorted, rightSorted);
+
+}
+
 //our best bet would be to choose a sorting technique and then apply it to linked lists
 ListNode *sortList(ListNode *head)
 {
@@ -60,10 +106,10 @@ int main()
     head->next->next = new ListNode(1);
     head->next->next->next = new ListNode(3);
     // head->next->next->next->next = new ListNode(34);
-    ListNode *ans = sortList(head);
-    while(head){
-        cout<<head->val<<"-->";
-        head=head->next;
+    ListNode *ans = sortlist(head);
+    while(ans){
+        cout<<ans->val<<"-->";
+        ans=ans->next;
     }
 }
 //by ad73prem
